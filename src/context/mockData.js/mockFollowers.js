9fc307e2e349b16c948b67aeb2d57fae531c9c0f
SergeAuck <sergey.auckland@gmail.com>
2023-05-24 19:50:34 +1200

    initial commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    login: "919971603657",
+    id: 19673568,
+    node_id: "MDQ6VXNlcjE5NjczNTY4",
+    avatar_url: "https://avatars0.githubusercontent.com/u/19673568?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/919971603657",
+    html_url: "https://github.com/919971603657",
+    followers_url: "https://api.github.com/users/919971603657/followers",
+    following_url:
+      "https://api.github.com/users/919971603657/following{/other_user}",
+    gists_url: "https://api.github.com/users/919971603657/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/919971603657/starred{/owner}{/repo}",
+    subscriptions_url:
+      "https://api.github.com/users/919971603657/subscriptions",
+    organizations_url: "https://api.github.com/users/919971603657/orgs",
+    repos_url: "https://api.github.com/users/919971603657/repos",
+    events_url: "https://api.github.com/users/919971603657/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/919971603657/received_events",
+    type: "User",
+    site_admin: false,
+  },
+];
